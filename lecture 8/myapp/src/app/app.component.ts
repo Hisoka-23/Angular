510d8f8be8aeb5e35976b8c8059d0582574de25c
prakash <prakash.chaurasiya214522@gmail.com>
2025-08-08 11:32:19 +0530

    angular lecture

---
+++ lecture 8/myapp/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  message:string="Welcome to home page!";
+  companyName:string="Ashokit services"
+  title = 'myapp';
+}
