9b52a1a518d968c0cf617105360804cc23a5614f
prakash <prakash.chaurasiya214522@gmail.com>
2025-10-24 18:58:06 +0530

    components

--- angular 19/angular19Tutorial/src/app/app.ts
+++ angular 19/angular19Tutorial/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Admin } from "./pages/admin/admin";
+import { User } from "./pages/user/user";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Admin, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ angular 19/angular19Tutorial/src/app/pages/user/user.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  template: `<h2>User Page</h2>`,
+  styles:[`h2 { color: green; }`]
+})
+export class User {
+
+}
